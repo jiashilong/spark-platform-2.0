9f8c1d579ef6297e139ae831058cc87e50a268c8
jiashilong <jiashilong@yongche.com>
2017-01-11 18:00:37 +0800

    update

--- src/main/scala/com/jarry/spark/hive/HiveSqlApp.scala
+++ src/main/scala/com/jarry/spark/hive/HiveSqlApp.scala
@@ -11,7 +11,11 @@ object HiveSqlApp {
         val sql = args(0)
 
         import spark.implicits._
-        spark.sql(sql).take(10).foreach(println)
+        val rs = spark.sql(sql).collect()
+        for(r <- rs) {
+            println(r.getString(0) + "," + r.getString(1) + "," + r.getString(2) + "," + r.getString(3))
+        }
+
         spark.close()
     }
 }
