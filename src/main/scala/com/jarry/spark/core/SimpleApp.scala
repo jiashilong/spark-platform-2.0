657e8d789f4fd7f7466972d872487a35b6e3a6f6
jiashilong <jiashilong@yongche.com>
2017-02-15 15:33:13 +0800

    update

--- src/main/scala/com/jarry/spark/core/SimpleApp.scala
+++ src/main/scala/com/jarry/spark/core/SimpleApp.scala
@@ -1,20 +1,21 @@
 package com.jarry.spark.core
 
+import com.jarry.spark.util.Logging
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by jarry on 16/12/29.
   */
-object SimpleApp {
+object SimpleApp extends Logging {
     def main(args: Array[String]):Unit = {
-        val logFile = args(0);
-        val conf = new SparkConf().setAppName("SimpleApp").setMaster("local[2]")
+        val logFile = "src/main/resources/data.txt"
+        val conf = new SparkConf().setAppName("SimpleApp").setMaster("local")
         val sc = new SparkContext(conf)
         val data = sc.textFile(logFile, 2).cache()
         val as = data.filter(_.contains("a")).count()
         val bs = data.filter(_.contains("b")).count()
 
-        println(s"Lines with a: $as, Lines with b: $bs")
+        logInfo(s"Lines with a: $as, Lines with b: $bs")
 
         sc.stop();
 
--- src/main/scala/com/jarry/spark/streaming/SocketReceiver.scala
+++ src/main/scala/com/jarry/spark/streaming/SocketReceiver.scala
@@ -6,9 +6,7 @@ import java.nio.charset.StandardCharsets
 
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.receiver.Receiver
-import org.slf4j.LoggerFactory
-import com.typesafe.scalalogging.slf4j.Logger
-import com.jarry.spark.util.NextIterator
+import com.jarry.spark.util.{Logging, NextIterator}
 
 import scala.util.control.NonFatal
 
@@ -16,18 +14,19 @@ import scala.util.control.NonFatal
 /**
   * Created by jarry on 17/1/16.
   */
-class SocketReceiver(host:String, port:Int) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) {
-    @transient lazy val log = Logger(LoggerFactory.getLogger(this.getClass))
+class SocketReceiver(host:String, port:Int)
+    extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2)
+    with Logging {
     private var socket: Socket = _
 
     override def onStart(): Unit = {
-        log.info(s"Connecting to $host:$port")
+        logInfo(s"Connecting to $host:$port")
         try {
             socket = new Socket(host, port)
         } catch {
             case e: ConnectException => {
                 restart(s"Restart, Error connect to $host:$port", e)
-                log.error(s"Error connect to $host:$port, Restart")
+                logError(s"Error connect to $host:$port, Restart")
                 return
             }
         }
@@ -48,7 +47,7 @@ class SocketReceiver(host:String, port:Int) extends Receiver[String](StorageLeve
             if(socket != null) {
                 socket.close()
                 socket = null
-                log.info(s"Closed socket from $host:$port")
+                logInfo(s"Closed socket from $host:$port")
             }
         }
     }
@@ -78,13 +77,13 @@ class SocketReceiver(host:String, port:Int) extends Receiver[String](StorageLeve
 
             if(!isStopped()) {
                 restart("Restart, Socket data stream had no more data")
-                log.info("Socket data stream had no more data, Restart")
+                logInfo("Socket data stream had no more data, Restart")
             } else {
-                log.info("Stopped receiving")
+                logInfo("Stopped receiving")
             }
         } catch {
             case NonFatal(e) => {
-                log.error("Restart, Error receiving data", e)
+                logError("Restart, Error receiving data", e)
                 restart("Restart, Error receiving data", e)
             }
         } finally {
